e418f7c0d0844feb848770992647abff5a6a9005
Akhil B Menon <59950744+abm2707@users.noreply.github.com>
2023-03-28 15:01:49 +0530

    Add files via upload

---
+++ Amazon22/sample.py
@@ -0,0 +1,10 @@
+from afinn import Afinn
+
+# Initialize an Afinn object
+afinn = Afinn()
+
+# Get the path to the AFINN lexicon file
+path_to_afinn = afinn.path
+
+# Print the path to the AFINN lexicon file
+print("Path to AFINN lexicon file:", path_to_afinn)
